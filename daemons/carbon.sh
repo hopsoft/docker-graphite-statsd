11f39b796b74299a727b94254cb96ce9ec7660f8
ddragosd <ddragosd@gmail.com>
2014-12-11 17:40:41 -0800

    added carbon-aggreagator proces
    Carbon-aggregator was not starting by default

--- daemons/carbon.sh
+++ daemons/carbon.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 /usr/bin/python /opt/graphite/bin/carbon-cache.py start --debug --logdir=/var/log/carbon 2>&1 >> /var/log/carbon.log
+/usr/bin/python /opt/graphite/bin/carbon-aggregator.py start --debug --logdir=/var/log/carbon 2>&1 >> /var/log/carbon-aggregator.log
