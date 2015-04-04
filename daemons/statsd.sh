25c24fe3c5e81963dc292d0350d3ecdb6e722603
Zaiming Shi <zmstone@gmail.com>
2015-04-04 02:38:52 -0400

    revert log directories

--- daemons/statsd.sh
+++ daemons/statsd.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-/usr/bin/nodejs /opt/statsd/stats.js /opt/statsd/config.js 2>&1 >> /var/log/statsd/statsd.log
+/usr/bin/nodejs /opt/statsd/stats.js /opt/statsd/config.js 2>&1 >> /var/log/statsd.log
 
--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -14,7 +14,7 @@ if [ ! -f /opt/graphite/storage/graphite.db ]; then
 fi
 
 # ensure log directories
-mkdir -p /var/log/carbon /var/log/graphite /var/log/nginx /var/log/statsd
+mkdir -p /var/log/graphite /var/log/nginx
 
 if [ "$1" == "my_init" ]; then
   /sbin/my_init
