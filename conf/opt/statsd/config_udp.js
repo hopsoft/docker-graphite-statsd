96438bd4dbfa7a3677be4d5eb9c1aecba32c6cb7
Dan Cech <dcech@grafana.com>
2017-10-26 10:19:06 -0400

    update statsd to v0.8.0, support statsd listening on tcp

---
+++ conf/opt/statsd/config_udp.js
@@ -0,0 +1,9 @@
+{
+  "graphiteHost": "127.0.0.1",
+  "graphitePort": 2003,
+  "port": 8125,
+  "flushInterval": 10000,
+  "servers": [
+    { server: "./servers/udp", address: "0.0.0.0", port: 8125 }
+  ]
+}
