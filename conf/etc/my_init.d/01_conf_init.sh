73094ab3c7e207c4f7954ed57fcea689361f1f1c
Denys Zhdanov <denis.zhdanov@gmail.com>
2018-01-17 11:52:54 +0100

    Fixing https://github.com/graphite-project/docker-graphite-statsd/issues/18

--- conf/etc/my_init.d/01_conf_init.sh
+++ conf/etc/my_init.d/01_conf_init.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 conf_dir=/etc/graphite-statsd/conf
 
@@ -11,12 +11,16 @@ graphite_dir_contents=$(find /opt/graphite -mindepth 1 -print -quit)
 graphite_conf_dir_contents=$(find /opt/graphite/conf -mindepth 1 -print -quit)
 graphite_webapp_dir_contents=$(find /opt/graphite/webapp/graphite -mindepth 1 -print -quit)
 graphite_storage_dir_contents=$(find /opt/graphite/storage -mindepth 1 -print -quit)
+graphite_log_dir_contents=$(find /var/log/graphite -mindepth 1 -print -quit)
+graphite_custom_dir_contents=$(find /opt/graphite/webapp/graphite/functions/custom -mindepth 1 -print -quit)
+if [[ -z $graphite_log_dir_contents ]]; then
+  mkdir -p /var/log/graphite
+  chown graphite:graphite /var/log/graphite
+  touch /var/log/syslog
+fi
 if [[ -z $graphite_dir_contents ]]; then
   # git clone -b 1.0.2 --depth 1 https://github.com/graphite-project/graphite-web.git /usr/local/src/graphite-web
   cd /usr/local/src/graphite-web && python ./setup.py install
-fi
-if [[ -z $graphite_storage_dir_contents ]]; then
-  /usr/local/bin/django_admin_init.exp
 fi
 if [[ -z $graphite_conf_dir_contents ]]; then
   cp -R $conf_dir/opt/graphite/conf/*.conf /opt/graphite/conf/
@@ -24,7 +28,12 @@ fi
 if [[ -z $graphite_webapp_dir_contents ]]; then
   cp $conf_dir/opt/graphite/webapp/graphite/local_settings.py /opt/graphite/webapp/graphite/local_settings.py
 fi
-
+if [[ -z $graphite_storage_dir_contents ]]; then
+  /usr/local/bin/django_admin_init.exp
+fi
+if [[ -z $graphite_custom_dir_contents ]]; then
+  touch /opt/graphite/webapp/graphite/functions/custom/__init__.py
+fi
 # auto setup statsd with default config if /opt/statsd is missing
 # needed for the use case when a docker host volume is mounted at an of the following:
 #  - /opt/statsd
