bc021e18f64a65bf18e12824072acf7129d2d753
deniszh <denis.zhdanov@gmail.com>
2018-10-14 00:45:56 +0200

    Switching to virtualenv

--- conf/usr/local/bin/manage.sh
+++ conf/usr/local/bin/manage.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Django 1.11
-PYTHONPATH=/opt/graphite/webapp django-admin.py makemigrations --settings=graphite.settings
-PYTHONPATH=/opt/graphite/webapp django-admin.py migrate auth --settings=graphite.settings
-PYTHONPATH=/opt/graphite/webapp django-admin.py migrate --run-syncdb --settings=graphite.settings
-PYTHONPATH=/opt/graphite/webapp django-admin.py createsuperuser --settings=graphite.settings
+PYTHONPATH=/opt/graphite/webapp /opt/graphite/bin/django-admin.py makemigrations --settings=graphite.settings
+PYTHONPATH=/opt/graphite/webapp /opt/graphite/bin/django-admin.py migrate auth --settings=graphite.settings
+PYTHONPATH=/opt/graphite/webapp /opt/graphite/bin/django-admin.py migrate --run-syncdb --settings=graphite.settings
+PYTHONPATH=/opt/graphite/webapp /opt/graphite/bin/django-admin.py createsuperuser --settings=graphite.settings
 
 # Django 1.8
 # PYTHONPATH=/opt/graphite/webapp django-admin.py syncdb --settings=graphite.settings
